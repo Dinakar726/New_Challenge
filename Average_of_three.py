b305cdc883d446dd1ba20b9d3b7008ed1e68e0f1
Dinakar726 <62534745+Dinakar726@users.noreply.github.com>
2021-02-26 10:34:04 +0530

    Add files via upload

--- Average_of_three.py
+++ Average_of_three.py
@@ -7,3 +7,9 @@ number3=int(input("Enter number3:"))
 total=int(number1+number2+number3)
 avg=total/3
 print(avg)
+# -----------------------------------------------------------------------------------
+
+number1,number2,number3=int(input("Enter number1:")),int(input("Enter numnber2:")),int(input("Enter number3:"))
+total=number1+number2+number3
+avg=total/3
+print(avg,type(avg))
